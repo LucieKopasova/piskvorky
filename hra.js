67729497fb3cb87ff65574fd01aefde1368126af
Lucie Kopasová <luca.kopasova@seznam.cz>
2025-04-13 15:51:10 +0200

    uprava proměných

--- hra.js
+++ hra.js
@@ -1,34 +1,31 @@
 let currentPlayer = 'circle';
 
-const playFieldEl = document.querySelectorAll('.play-field');
+const playFieldsEl = document.querySelectorAll('.play-field'); // pojmenoval bych
 let playSymbolEl = document.querySelector('#player__symbol');
 
 const addcircle = (e) => {
   if (currentPlayer === 'circle') {
     e.target.classList.add('board__field--circle');
-    setTimeout(() => {
-      e.target.classList.add('expand');
-    }, 10);
-
     currentPlayer = 'cross';
     setTimeout(() => {
       playSymbolEl.src = 'images/cross--white.svg';
     }, 300);
   } else if (currentPlayer === 'cross') {
     e.target.classList.add('board__field--cross');
-    setTimeout(() => {
-      e.target.classList.add('expand');
-    }, 10);
     currentPlayer = 'circle';
     setTimeout(() => {
       playSymbolEl.src = 'images/circle--white.svg';
     }, 300);
   }
+
+  setTimeout(() => {
+    e.target.classList.add('expand');
+  }, 10);
   e.target.disabled = true;
 };
 
-playFieldEl.forEach((addclick) => {
-  addclick.addEventListener('click', addcircle);
+playFieldsEl.forEach((field) => {
+  field.addEventListener('click', addcircle);
 });
 
 document.querySelector('.play-again').addEventListener('click', () => {

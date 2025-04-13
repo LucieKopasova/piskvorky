e766e5f68e3fdf61e84ed45287b3dba24a13e015
Lucie Kopasová <luca.kopasova@seznam.cz>
2025-04-13 15:32:35 +0200

    přidání animace

--- hra.js
+++ hra.js
@@ -6,12 +6,19 @@ let playSymbolEl = document.querySelector('#player__symbol');
 const addcircle = (e) => {
   if (currentPlayer === 'circle') {
     e.target.classList.add('board__field--circle');
+    setTimeout(() => {
+      e.target.classList.add('expand');
+    }, 10);
+
     currentPlayer = 'cross';
     setTimeout(() => {
       playSymbolEl.src = 'images/cross--white.svg';
     }, 300);
   } else if (currentPlayer === 'cross') {
     e.target.classList.add('board__field--cross');
+    setTimeout(() => {
+      e.target.classList.add('expand');
+    }, 10);
     currentPlayer = 'circle';
     setTimeout(() => {
       playSymbolEl.src = 'images/circle--white.svg';

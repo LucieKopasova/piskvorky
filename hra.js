d486ae35950aea18ce9c9c092f4bbb2cd9f0a8f0
Lucie Kopasová <luca.kopasova@seznam.cz>
2025-05-18 21:34:07 +0200

    přidání gamerAI

--- hra.js
+++ hra.js
@@ -5,7 +5,7 @@ let currentPlayer = 'circle';
 const playFieldsEl = document.querySelectorAll('.play-field');
 let playSymbolEl = document.querySelector('#player__symbol');
 
-const addcircle = (e) => {
+const addcircle = async (e) => {
   if (currentPlayer === 'circle') {
     e.target.classList.add('board__field--circle');
     currentPlayer = 'cross';
@@ -25,7 +25,7 @@ const addcircle = (e) => {
   }, 10);
   e.target.disabled = true;
 
-  const playAreaStatuts = Array.from(
+  const playAreaStatus = Array.from(
     document.querySelectorAll('.play-field'),
   ).map((field) => {
     if (field.classList.contains('board__field--circle')) {
@@ -37,9 +37,9 @@ const addcircle = (e) => {
     }
   });
 
-  console.log(playAreaStatuts);
+  console.log(playAreaStatus);
 
-  const winner = findWinner(playAreaStatuts);
+  const winner = findWinner(playAreaStatus);
   if (winner === 'o' || winner === 'x') {
     playFieldsEl.forEach((field) => {
       field.classList.add('game-over');
@@ -48,11 +48,42 @@ const addcircle = (e) => {
       alert(`Vyhrál hráč se symbolem ${winner}.`);
       window.location.reload();
     }, 400);
-  } else if (!playAreaStatuts.includes('_')) {
+  } else if (!playAreaStatus.includes('_')) {
     setTimeout(() => {
       alert(`Hra skončila nerozhodně.`);
       window.location.reload();
     }, 400);
+  } else if (currentPlayer === 'cross') {
+    playFieldsEl.forEach((field) => {
+      field.disabled = true;
+    });
+    const response = await fetch(
+      'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          board: playAreaStatus,
+          player: 'x',
+        }),
+      },
+    );
+
+    const data = await response.json();
+    const { x, y } = data.position;
+    const field = playFieldsEl[x + y * 10];
+
+    playFieldsEl.forEach((field) => {
+      if (
+        !field.classList.contains('board__field--circle') &&
+        !field.classList.contains('board__field--cross')
+      ) {
+        field.disabled = false;
+      }
+    });
+    field.click();
   }
 };
 
@@ -60,11 +91,11 @@ playFieldsEl.forEach((field) => {
   field.addEventListener('click', addcircle);
 });
 
-document.querySelector('.play-again').addEventListener('click', () => {
+document.querySelector('.play-again').addEventListener('click', (e) => {
   const userConfirm = confirm('Opravdu chceš začít znovu?');
   if (userConfirm === true) {
     window.location.reload();
   } else if (userConfirm === false) {
-    event.preventDefault();
+    e.preventDefault();
   }
 });
